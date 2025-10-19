2666632b3c007a4526edadd804754e6db0273f83
Aleksandra718 <a71m8r@gmail.com>
2025-10-19 12:07:36 +0500

    feat(admin): добавить панель администратора

--- main/admin.py
+++ main/admin.py
@@ -1,3 +1,23 @@
 from django.contrib import admin
+from .models import Category, Product, ProductImage 
 
-# Register your models here.
+class ProductImageInline(admin.TabularInline):
+    model = ProductImage
+    extra = 1
+
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ['name', 'category', 'price']
+    list_filter = ['category']
+    search_fields = ['name', 'category']
+    prepopulated_fields = {'slug': ('name',)}
+    inlines = [ProductImageInline]
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ['name', 'slug']
+    prepopulated_fields = {'slug': ('name',)}
+
+
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(Product, ProductAdmin)
